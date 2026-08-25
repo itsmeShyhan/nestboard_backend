16a922c24b4723a66f8ceabf817351a074bc9d80
itsmeshyhan <shyhanwahab@gmail.com>
2026-08-25 18:58:06 +0530

    changed import again

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import { pinoHttp } from "pino-http";
 import { logger } from "./lib/logger.js";
 import { errorHandler } from "./middleware/error-handler.js";
 import { authRouter } from "./routes/auth.js";
-import {default as helmet} from "helmet";
+import * as helmetModule from "helmet";
 import cors from "cors";
 import { corsOrigins, env } from "./lib/env.js";
 import { bookingsRouter } from "./routes/bookings.js";
@@ -14,6 +14,8 @@ import path from "node:path";
 import {rateLimit} from "express-rate-limit";
 
 export function buildApp(): Express {
+
+  const helmet = helmetModule.default;
   const app = express();
 
   // behind a reverse proxy; express-rate-limit and req.ip need X-Forwarded-For honored
